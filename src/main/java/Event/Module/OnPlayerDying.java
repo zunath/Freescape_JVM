dff50bd60084fdbe4d3918751f9416675a63d90d
zunath <coolty3001@yahoo.com>
2018-03-19 18:58:33 +0800

    Fix an issue with the dying script that was preventing HP from being healing on stabilization. Also fixes a crash issue related to logging out at -9 hp and logging back in.

--- src/main/java/Event/Module/OnPlayerDying.java
+++ src/main/java/Event/Module/OnPlayerDying.java
@@ -4,53 +4,56 @@ import Common.IScriptEventHandler;
 import org.nwnx.nwnx2.jvm.*;
 import org.nwnx.nwnx2.jvm.constants.*;
 
+import static org.nwnx.nwnx2.jvm.NWScript.*;
+
 @SuppressWarnings("unused")
 public class OnPlayerDying implements IScriptEventHandler {
 	@Override
 	public void runScript(NWObject objSelf) {
+		final NWObject oPC = getLastPlayerDying();
 
-		final NWObject oPC = NWScript.getLastPlayerDying();
+		Scheduler.assign(oPC, () -> clearAllActions(false));
 
-		Scheduler.assign(oPC, () -> {
-            NWScript.clearAllActions(false);
-            DeathFunction(oPC, 8);
-        });
+		DeathFunction(oPC, 8);
 	}
 
 
 	private void DeathFunction(final NWObject oPC, int nDC) {
-		int iHP = NWScript.getCurrentHitPoints(oPC);
-		if (!NWScript.getIsObjectValid(oPC)) return;
+		if (!getIsObjectValid(oPC)) return;
+		int iHP = getCurrentHitPoints(oPC);
 
 		//Player Rolls a random number between 1 and 20+ConMod
-		int iRoll = 20 + NWScript.getAbilityModifier(Ability.CONSTITUTION, oPC);
-		iRoll = NWScript.random(iRoll) + 1;
+		int iRoll = 20 + getAbilityModifier(Ability.CONSTITUTION, oPC);
+		iRoll = random(iRoll) + 1;
 
 		//Sanity Check
 		if (nDC > 30) nDC = 30;
 		else if (nDC < 4) nDC = 4;
 
 		NWEffect eResult;
-		if (iHP <= 0) {
+		if (iHP <= 0)
+		{
 			if (iRoll >= nDC) //Stabilize
 			{
 				nDC -= 2;
-				eResult = NWScript.effectHeal(1);
-				//PlayVoiceChat(VOICE_CHAT_LAUGH);
-			} else           //Failed!
+				applyEffectToObject(DurationType.INSTANT, effectHeal(1), oPC, 0.0f);
+				final int dcCopy = nDC;
+				Scheduler.delay(oPC, 3000, () -> DeathFunction(oPC, dcCopy));
+			}
+			else  //Failed!
 			{
-				if (NWScript.random(2) + 1 == 1) nDC++;
-				eResult = NWScript.effectDamage(1, DamageType.MAGICAL, DamagePower.NORMAL);
+				if (random(2) + 1 == 1) nDC++;
+				eResult = effectDamage(1, DamageType.MAGICAL, DamagePower.NORMAL);
 
 				//Death!
 				if (iHP <= -9) {
-					NWScript.applyEffectToObject(DurationType.INSTANT, NWScript.effectVisualEffect(Vfx.IMP_DEATH, false), oPC, 0.0f);
-					NWScript.applyEffectToObject(DurationType.INSTANT, NWScript.effectDeath(false, true), oPC, 0.0f);
+					applyEffectToObject(DurationType.INSTANT, effectVisualEffect(Vfx.IMP_DEATH, false), oPC, 0.0f);
+					applyEffectToObject(DurationType.INSTANT, effectDeath(false, true), oPC, 0.0f);
 					return;
 				} else {
-					NWScript.sendMessageToPC(oPC, ColorToken.Orange() + "You failed to stabilize this round." + ColorToken.End());
+					sendMessageToPC(oPC, ColorToken.Orange() + "You failed to stabilize this round." + ColorToken.End());
 				}
-				NWScript.applyEffectToObject(DurationType.INSTANT, eResult, oPC, 0.0f);
+				applyEffectToObject(DurationType.INSTANT, eResult, oPC, 0.0f);
 
 				final int dcCopy = nDC;
 				Scheduler.delay(oPC, 3000, () -> DeathFunction(oPC, dcCopy));
